73585c5efd61ea13180a441d2f5ec6a9e72b6b70
Brian McDonald <bmcdonald916@gmail.com>
2021-05-11 09:05:00 -0400

    Use the discord.py framework for commands
    This commit refactors the commands handled by the bot to use the
    discord.py commands framework rather than just checking messages that
    come in manually. This gets rid of some ugly code that was checking the
    incoming messages for valid commands and leaves meme_bot.py pretty much
    exclusively code that is setting up or directly handling commands.
    
    The help command text formatting was also improved during these changes,
    although the bot is not using the default help command from discord.py.

--- src/meme_bot.py
+++ src/meme_bot.py
@@ -1,13 +1,15 @@
 import datetime
+import inspect
 import os
 import pathlib
 import random
 import re
 
 import discord
+from discord.ext import commands
 
 
-class MemeBot(discord.Client):
+class MemeBot(commands.Bot):
     """
     A discord bot that responds to various text commands with different memes.
 
@@ -20,20 +22,16 @@ class MemeBot(discord.Client):
         The PersonalPicksScraper that provides access to the personal picks YouTube playlist.
     """
 
-    HELP_COMMAND_RE = re.compile(r'^\s*!help\s*$')
-    MEME_COMMAND_RE = re.compile(r'^\s*!meme\s*$')
-    DOMAN_COMMAND_RE = re.compile(r'^\s*!doman\s*$')
-    HELP_TEXT = '\n'.join([
-        'Available meme commands:',
-        '!help - Shows this list of commands',
-        '!meme - Responds with one of the internet\'s finest memes',
-        '!doman - Responds with a random video from the world famous playlist Johnathan Doman\'s Personal Picks'
-    ])
-
     def __init__(self, reddit_scraper, personal_picks_scraper):
         self.reddit_scraper = reddit_scraper
         self.personal_picks_scraper = personal_picks_scraper
-        super().__init__()
+        super().__init__(command_prefix="!", help_command=None)
+        self.add_command(commands.Command(self.help, name='help',
+                                          help='Shows this help message'))
+        self.add_command(commands.Command(self.meme, name='meme',
+                                          help='Responds with one of the internet\'s finest memes'))
+        self.add_command(commands.Command(self.doman, name='doman',
+                                          help='Responds with a video from Johnathan Doman\'s Personal Picks'))
         self.log('Created Meme Bot')
 
     async def on_ready(self):
@@ -43,32 +41,65 @@ class MemeBot(discord.Client):
             type=discord.ActivityType.watching)
         await self.change_presence(activity=activity)
 
-    async def on_message(self, message):
-        """Called when a new message is sent in the Discord."""
-        if message.author == self.user:
-            return
-        if MemeBot.HELP_COMMAND_RE.match(message.content) is not None:
-            self.log(f'Got help request from {message.author.name}')
-            await message.channel.send(MemeBot.HELP_TEXT)
-        elif MemeBot.MEME_COMMAND_RE.match(message.content) is not None:
-            self.log(f'Got meme request from {message.author.name}')
-            meme_file = self.reddit_scraper.get_random_meme_file()
-            if meme_file is None:
-                self.log('No memes found to reply with')
-                await message.channel.send('I\'m sorry. I couldn\'t find a meme for you.')
-            else:
-                self.log(f'Replying with meme {meme_file}')
-                await message.channel.send(file=discord.File(meme_file))
-        elif MemeBot.DOMAN_COMMAND_RE.match(message.content) is not None:
-            self.log(
-                f'Got Johnathan Doman\'s Personal Picks request from {message.author.name}')
-            video_url = self.personal_picks_scraper.get_random_video_url()
-            if video_url is None:
-                self.log('No video found to reply with')
-                await message.channel.send('I\'m sorry. I don\'t have a personal pick for you right now.')
-            else:
-                self.log(f'Replying with meme {video_url}')
-                await message.channel.send(video_url)
+    async def help(self, ctx):
+        """
+        Handles the help command by responding with the list of available commands.
+
+        Parameters
+        ----------
+
+        ctx:
+            The context provided by the commands API.
+        """
+        commands = [
+            self.get_command('doman'),
+            self.get_command('help'),
+            self.get_command('meme')
+        ]
+        name_col_length = max([len(c.name) for c in commands]) + 1
+        help_lines = [
+            f'\t{self.command_prefix}{c.name.ljust(name_col_length)} {c.help}' for c in commands]
+        help_text = 'Meme Commands:\r\n' + '\r\n'.join(help_lines)
+        await ctx.channel.send(f'```{help_text}```')
+
+    async def meme(self, ctx):
+        """
+        Handles the meme command by responding with a random meme.
+
+        Parameters
+        ----------
+
+        ctx:
+            The context provided by the commands API.
+        """
+        self.log(f'Got meme request from {ctx.message.author.name}')
+        meme_file = self.reddit_scraper.get_random_meme_file()
+        if meme_file is None:
+            self.log('No memes found to reply with')
+            await ctx.channel.send('I\'m sorry. I couldn\'t find a meme for you.')
+        else:
+            self.log(f'Replying with meme {meme_file}')
+            await ctx.channel.send(file=discord.File(meme_file))
+
+    async def doman(self, ctx):
+        """
+        Handles the doman command by responding with a random video from the personal picks YouTube playlist.
+
+        Parameters
+        ----------
+
+        ctx:
+            The context provided by the commands API.
+        """
+        self.log(
+            f'Got Johnathan Doman\'s Personal Picks request from {ctx.message.author.name}')
+        video_url = self.personal_picks_scraper.get_random_video_url()
+        if video_url is None:
+            self.log('No video found to reply with')
+            await ctx.channel.send('I\'m sorry. I don\'t have a personal pick for you right now.')
+        else:
+            self.log(f'Replying with meme {video_url}')
+            await ctx.channel.send(video_url)
 
     def log(self, message):
         """
